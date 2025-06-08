ce97dd7ad5b02c479ffb3ccc47662c384c19c73e
Clifford Laserre <claserre9@gmail.com>
2025-06-08 01:31:27 -0400

    Add Twig DebugExtension and update .gitignore for IDE files

--- config/container.php
+++ config/container.php
@@ -2,11 +2,14 @@
 
 
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 $definitions = [
-    FilesystemLoader::class => DI\create()->constructor(__DIR__ . "/../templates"),
-    Environment::class => DI\create()->constructor(DI\get(FilesystemLoader::class)),
+    DebugExtension::class =>DI\autowire(),
+    FilesystemLoader::class => DI\autowire()->constructor(__DIR__ . "/../templates"),
+    Environment::class => DI\autowire()->constructor(DI\get(FilesystemLoader::class))
+        ->method('addExtension', DI\get(DebugExtension::class)),
 ];
 
 return $definitions;
